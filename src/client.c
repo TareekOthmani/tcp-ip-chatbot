0ae72387019c686611c33974c8514a95784fe99d
rek516 <tareekothmani@gmail.com>
2020-08-16 17:17:23 +0100

    return

--- src/client.c
+++ src/client.c
@@ -25,7 +25,7 @@ int main()
 
 	// assign IP, PORT 
 	servaddr.sin_family = AF_INET; 
-	servaddr.sin_addr.s_addr = inet_addr("127.0.0.1"); 
+	servaddr.sin_addr.s_addr = inet_addr("51.178.18.86"); 
 	servaddr.sin_port = htons(PORT); 
 
 	// connect the client socket to server socket 
